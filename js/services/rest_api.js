d45cff39ab1ee5013d791b2a04287505f9ab64c2
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-09 03:32:43 -0400

    implemented service POST , GET

--- js/services/rest_api.js
+++ js/services/rest_api.js
@@ -1,7 +1,8 @@
 
 
 
-const itemsCards = document.getElementById('itemsCard')
+const itemsCards = document.getElementById('itemsCard');
+const formAddItem = document.getElementById('formAddItem')
 
 
 
@@ -12,15 +13,67 @@ fetch('https://clothesinventory.up.railway.app/items/getAll')
         showData(data)
     });
 
-
 const showData = (data) => {
 
     console.log(data)
 
     let body = ''
     for( let i = 0 ; i < data.length ; i++){
-        body += `<tr><td>${data[i]._id}</td><td>${data[i].name}</td><td>${data[i].type}</td><td>${data[i].size}</td><td>${data[i].quantity}</td><td>${data[i].entryDate}</td><td>${data[i].exitDate}</td></tr>`
+        body += `<tr>
+                <td>${data[i]._id}</td>
+                <td>${data[i].name}</td>
+                <td>${data[i].type}</td>
+                <td>${data[i].size}</td>
+                <td>${data[i].quantity}</td>
+                <td>${data[i].entryDate}</td>
+                <td>${data[i].exitDate}</td>
+                </tr>`
     }
 
     document.getElementById('data').innerHTML = body
 }
+
+
+
+
+formAddItem.addEventListener('submit',function (e){
+    e.preventDefault();
+
+    const name_ = document.getElementById('input-nombre').value;
+    const type_ = document.getElementById('inputType').value;
+    const size_ = document.getElementById('inputSize').value;
+    const quantity_ = document.getElementById('inputQuantity').value;
+    const entryDate_ = '2023-12-01';
+    const exitDate_ = '2023-12-01';
+
+    fetch('https://clothesinventory.up.railway.app/items/addItem' ,{
+        method:'POST',
+        body:JSON.stringify({
+            name:name_,
+            type:type_,
+            size:size_,
+            quantity:quantity_,
+            entryDate:entryDate_,
+            exitDate:exitDate_
+        }),
+        headers:{
+            "Content-Type":"application/json; charset=UTF-8"
+        }
+    })
+        .then(function (response){
+            return response.json()
+        })
+        .then(function (data){
+            console.log(data)
+        })
+
+
+
+});
+
+
+
+
+
+
+
